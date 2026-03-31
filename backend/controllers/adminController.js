c0124a2e131c6fafccfa9d8d3b5d70d395bfc0bc
Chikun17 <krushnachandran33@gmail.com>
2026-03-31 23:33:36 +0530

    updated

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -62,5 +62,33 @@ const addDoctor = async (req, res) => {
     res.json({ success: false, message: error.message })
   }
 }
+//Api for admin login
+const loginAdmin= async(req,res) =>{
+    try{
+        const {email,password} = req.body
+        if (
+            email === process.env.ADMIN_EMAIL &&
+            password === process.env.ADMIN_PASSWORD
+          ) {
+            const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+              expiresIn: '1d',
+            });
+      
+            res.json({
+              success: true,
+              token,
+            });
+          } else {
+            res.json({
+              success: false,
+              message: "Invalid credentials",
+            });
+          }
+    }catch(error){
+        console.log(error)
+        res.json({success:false,message:error.message})
 
-export { addDoctor }
+    }
+}
+
+export { addDoctor ,loginAdmin}
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 // 
 
 import express from 'express'
-import { addDoctor } from '../controllers/adminController.js'
+import { addDoctor,loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
 
-const router = express.Router()
+const adminRouter = express.Router()
 
 // Note: 'image' must match the frontend field name
-router.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/login', loginAdmin)
 
-export default router
+export default adminRouter
